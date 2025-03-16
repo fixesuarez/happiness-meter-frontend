889f39514c5a3f304383eeb5dd9497bdf9f5cb1a
François-Xavier Suarez <francoisxaviersuarez@gmail.com>
2025-03-16 20:41:48 +0100

    Score are fetch only after dbUser is fetched

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,13 +3,17 @@ import ScoreForm from "@/components/ScoreForm";
 import { RootState } from "@/store";
 import { get } from "@/utils/httpWrapper";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 function Profile() {
   const { user } = useAuth0();
   const currentUser = useSelector((state: RootState) => state.user.currentUser);
 
-  void get(`/scores/${currentUser?._id}/`);
+  useEffect(() => {
+    if (!currentUser) return;
+    void get(`/scores/${currentUser?._id}/`);
+  }, [currentUser]);
 
   return (
     <>
