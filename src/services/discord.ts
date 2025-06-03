05c406fa6365189ba2116a7414eeaa051b5d3507
François-Xavier Suarez <francoisxaviersuarez@gmail.com>
2025-06-03 18:50:41 +0200

    Deactivate discord messages for now

--- src/services/discord.ts
+++ src/services/discord.ts
@@ -3,7 +3,9 @@ import { User } from "@/models/user";
 import { postToExternalService } from "@/utils/httpWrapper";
 
 export const sendScoreOnDiscord = (user: User, userScore: UserScore) => {
-  if (!import.meta.env.PROD) return;
+  // Todo: remove feature flag when ready for production
+  const featureFlag = false;
+  if (featureFlag && !import.meta.env.PROD) return;
 
   void postToExternalService(import.meta.env.VITE_DISCORD_URL, {
     content: `**${user.name}**: ${userScore.score}/10\n${userScore.comment ? `**Commentaire**: ${userScore.comment}` : ""}`,
